1a860545ce3880c57583c04df9fb94cec5dd1883
Naveed Aheer <naveedaheer@gmail.com>
2020-02-06 12:19:33 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CustomTreeView from './components/customTreeView/customTreeView';
+
+function App() {
+  return (
+    <div className="App">
+<CustomTreeView />    
+    </div>
+  );
+}
+
+export default App;
